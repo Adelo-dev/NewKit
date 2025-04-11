4b153fb8005269ff87fa7fed3881fd77088e44ef
Amir shahaf <irmir10@gmail.com>
2025-04-11 13:42:45 +0300

    DEV-15: Added a batch process script

---
+++ batch_process.py
@@ -0,0 +1,22 @@
+import os
+from glob import glob
+from tqdm import tqdm
+from inferencers.image_inferencer import ImageInference
+from inferencers.video_inferencer import VideoInferencer
+
+
+def main():
+    image_inference = ImageInference()
+    video_inference = VideoInferencer()
+    for image in tqdm(glob("input/*.jpg"), desc="Processing images"):
+        image_inference.inference(image_path=image,
+                                  output_path=f"output/{os.path.basename(image)}",
+                                  show=False)
+
+    for video in tqdm(glob("input/*.mp4"), desc="Processing videos"):
+        video_inference.inference(stream_path=video,
+                                  output_path=f"output/{os.path.basename(video)}",
+                                  show=False)
+
+if __name__ == "__main__":
+    main()
